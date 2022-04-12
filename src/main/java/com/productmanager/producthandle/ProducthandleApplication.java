95c0c294ba2bdd5538c201101c493eab15285cdc
daham.embuldeniya@axiatadigitallabs.com <daham.embuldeniya@axiatadigitallabs.com>
2022-04-12 14:26:49 +0530

    init

---
+++ src/main/java/com/productmanager/producthandle/ProducthandleApplication.java
@@ -0,0 +1,13 @@
+package com.productmanager.producthandle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProducthandleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProducthandleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/productmanager/producthandle/ProducthandleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.productmanager.producthandle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProducthandleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
